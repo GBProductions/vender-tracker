293952296e3b7ca4162351eb331a61c7ab3343a3
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-18 10:39:03 -0800

    Fill code, fix typos.

---
+++ VendorTrackerTests/ModelTests/OrdersTracker.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+using System;
+
+namespace VendorTracker.Tests
+{
+}
---
+++ VendorTrackerTests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+using System;
+
+namespace VendorTracker.Tests
+{
+    [TestClass]
+    public class VendorTests
+    {
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor();
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+    }
+}
