b3198283dd92f58eefb358c045edf9f17938ecf2
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-18 16:28:38 -0800

    Change VendorController.

--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
 
 namespace VendorTracker.Controllers
 {
     public class VendorController : Controller
     {
 
-        [HttpGet()]
+        [HttpGet("/vendors")]
         public ActionResult Index()
         {
-            return View();
+            return View(allVendors);
         }
     }
 }
